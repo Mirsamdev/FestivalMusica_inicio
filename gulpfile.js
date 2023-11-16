efb69b50928c9826773bc637866fe5fc341ddf0f
Mirsandev <xmirandadevx@gmail.com>
2023-11-16 10:34:17 -0400

    npx run dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
-function tarea( done ) {
-  console.log('mi primera Chamba');
+const { src, dest, watch } = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+
+function css(done) {
+src("src/scss/app.scss")// Identificar el archivo de SASS
+.pipe(sass())// Compilarlo
+.pipe(dest("build/css")) // Almacenar en el disco duro
+
+  done() // Callback que avisa a gulp cuando llegamos al final 
+}
+
+function dev(done) {
+  watch("src/scss/app.scss", css)
+
   done();
 }
 
-exports.tarea = tarea;
+exports.css = css;
+exports.dev = dev;
