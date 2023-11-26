a9654d4e41c5475ed3e088394104382269f88e09
Mirsandev <xmirandadevx@gmail.com>
2023-11-26 10:09:26 -0400

    seccion boletos culminacion de la galeria

--- build/js/app.js
+++ build/js/app.js
@@ -18,7 +18,50 @@ function crearGaleria() {
         <img thumb/loading="lazy" width="200" height="300" src="src/img/${i}.jpg" alt="Imagen imagen vocalista">
         </picture>
     `;
+    
+    imagen.onclick = function() {
+      mostrarImagen(i) 
+    }
 
     galeria.appendChild(imagen);
   }
 }
+
+function mostrarImagen(id) {
+  const imagen = document.createElement('picture');
+  imagen.innerHTML = `
+  <picture>
+  <source srcset="build/img/grande/${id}.avif" type="image/avif">
+  <source srcset="build/img/grande/${id}.webp" type="image/webp">
+  <img thumb/loading="lazy" width="200" height="300" src="src/img/grande/${id}.jpg" alt="Imagen imagen vocalista">
+  </picture>
+`;
+
+// Crea el overlay con la imagen
+const overlay = document.createElement('DIV');
+overlay.appendChild(imagen);
+overlay.classList.add('overlay');
+overlay.onclick = function() {
+  const body = document.querySelector('body');
+body.classList.remove('fijar-body')
+  overlay.remove();
+}
+
+
+// Boton para cerrar el Modal
+const cerrarModal = document.createElement('P')
+cerrarModal.textContent = 'X';
+cerrarModal.classList.add('btn-cerrar');
+cerrarModal.onclick = function() {
+  const body = document.querySelector('body');
+body.classList.remove('fijar-body')
+  overlay.remove();
+}
+overlay.appendChild(cerrarModal);
+
+// Añadirlo al HTML
+const body = document.querySelector('body');
+body.appendChild(overlay);
+body.classList.add('fijar-body')
+}
+
