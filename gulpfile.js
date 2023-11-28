287536d9484a323f1473ff279bc515fe1d258e17
Mirsandev <xmirandadevx@gmail.com>
2023-11-28 19:38:56 -0400

    terminacion del proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const plumber = require('gulp-plumber');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps')
 
 // Imagenes
 const cache = require('gulp-cache')
@@ -13,12 +14,18 @@ const imagemin = require('gulp-imagemin');
 const webp = require ('gulp-webp');
 const avif = require('gulp-avif')
   
+// Javascript
+const terser = require('gulp-terser-js')
+
+
 
 function css(done) {
 src("src/scss/**/*.scss")// Identificar el archivo de SASS
+.pipe( sourcemaps.init())
 .pipe( plumber())
 .pipe( sass())// Compilarlo
 .pipe( postcss([ autoprefixer(), cssnano()]) )
+.pipe( sourcemaps.write('.'))
 .pipe( dest("build/css")) // Almacenar en el disco duro
 
   done() // Callback que avisa a gulp cuando llegamos al final 
@@ -60,12 +67,15 @@ function versionAvif ( done ) {
       done();
   }
 
-function javascript( done ) {
-  src('src/js/**/*.js')
-    .pipe(dest('build/js'))
+  function javascript() {
+    return src('src/js/**/*.js')
+      .pipe(sourcemaps.init())
+      .pipe(terser())
+      .pipe(sourcemaps.write('.'))
+      .pipe(dest('build/js'))
 
-    done();
-}
+      done()
+  }
 
 function dev( done ) {
   watch("src/scss/**/*.scss", css)
